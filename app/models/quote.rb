b1cee5fbf553f0d7013ff110fdbb315da62af3e3
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-06 10:46:11 +0200

    Replace 3 callback helpers in Quote model with broadcasts_to helper

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,10 +3,12 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit lambda {
-                        puts '**** after_create_commit ****'
-                        broadcast_prepend_later_to 'quotes'
-                      }
-  after_update_commit -> { broadcast_replace_later_to 'quotes' }
-  after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  # after_create_commit lambda {
+  #                       puts '**** after_create_commit ****'
+  #                       broadcast_prepend_later_to 'quotes'
+  #                     }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(_quote) { 'quotes' }, inserts_by: :prepend
 end
