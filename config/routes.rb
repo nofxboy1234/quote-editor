898101adfac8667a1907a6dbc743d255c9b1ac7c
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-06 11:48:53 +0200

    Make pages#home the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  # get 'pages/home'
+  root to: 'pages#home'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
