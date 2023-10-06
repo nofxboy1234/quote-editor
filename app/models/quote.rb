c1aa42d484542bbae500575a84dbf2525faff546
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-06 10:31:05 +0200

    Broadcast Quote deletion

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,4 +8,5 @@ class Quote < ApplicationRecord
                         broadcast_prepend_to 'quotes'
                       }
   after_update_commit -> { broadcast_replace_to 'quotes' }
+  after_destroy_commit -> { broadcast_remove_to 'quotes' }
 end
