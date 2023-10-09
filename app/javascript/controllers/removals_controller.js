60086154d8a0529090b8fd287e7507c1b4f4b43c
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-09 10:38:48 +0200

    Remove flash message from the DOM on animationend

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -1,7 +1,8 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="removals"
 export default class extends Controller {
-  connect() {
+  remove() {
+    this.element.remove();
   }
 }
