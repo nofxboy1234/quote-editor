bd0f5f1e76380abfdc5885dd685edb5e74f89942
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-09 15:30:43 +0200

    Update routes file with nested quotes/line_item_dates

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  # get 'pages/home'
   root to: 'pages#home'
-
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :quotes
+  # resources :quotes
+  
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
