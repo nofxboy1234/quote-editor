99a9f729eeb5d247e1f7ca2fd6ebb656d7fe500c
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-11 11:34:43 +0200

    Add associations and validations to LineItem and LineItemDate models

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,14 @@
 class LineItem < ApplicationRecord
   belongs_to :line_item_date
+
+  validates :name, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :unit_price, presence: true, numericality: { greater_than: 0 }
+
+  # The below line delegates the :quote method to the LineItem#line_item_date method.
+  # This makes the two following lines equivalent:
+  
+  # line_item.line_item_date.quote
+  # line_item.quote
+  delegate :quote, to: :line_item_date
 end
